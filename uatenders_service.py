1f759b8b0ce9a041e24f864d7e4fba5375588f9c
selurvedu <selurvedu@users.noreply.github.com>
2016-04-14 17:56:34 +0000

    Update uatenders_service.py
    Updated file was supplied via email.

--- uatenders_service.py
+++ uatenders_service.py
@@ -3,4 +3,6 @@
 def convert_uatenders_string_to_common_string(string):
     return {
         u"килограммы": u"кілограм",
+        u"ПДВ)": True,
+        u"Картонки": u"Картонні коробки",
     }.get(string, string)
