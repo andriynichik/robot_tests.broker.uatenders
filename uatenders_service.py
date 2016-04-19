61efb7b2f7f0db8c4cfbdf69f7c4e67f299a4613
andriynichik <andreynichik@gmail.com>
2016-04-19 22:16:15 +0300

    Update uatenders_service.py

--- uatenders_service.py
+++ uatenders_service.py
@@ -4,20 +4,21 @@ from iso8601 import parse_date
 from datetime import datetime, timedelta
 
 
-def get_all_uatenders_dates(period_interval=31):
-    now = datetime.now()
+def get_all_uatenders_dates(tender_data):
     return {
-        'EndPeriod': (now + timedelta(minutes=8)).strftime("%d.%m.%Y %H:%M"),
-        'StartDate': (now + timedelta(minutes=8)).strftime("%d.%m.%Y %H:%M"),
-        'EndDate': (now + timedelta(minutes=(8 + period_interval))).strftime("%d.%m.%Y %H:%M"),
+        'EndPeriod': tender_data['data']['enquiryPeriod']['endDate'] ,
+        'StartDate': tender_data['data']['tenderPeriod']['startDate'],
+        'EndDate':   tender_data['data']['tenderPeriod']['endDate'],
     }
 
 def convert_uatenders_string_to_common_string(string):
     return {
         u"килограммы": u"кілограм",
+        u"ПДВ)": True,
+        u"Картонки": u"Картонні коробки",
     }.get(string, string)
 
 def convert_datetime_for_delivery(isodate):
     iso_dt = parse_date(isodate)
-    date_string = iso_dt.strftime("%Y-%m-%d %H:%M")
+    date_string = iso_dt.strftime("%d-%m-%Y %H:%M")
     return date_string
